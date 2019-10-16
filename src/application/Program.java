165a80cafce156adfa05277621c6ab4a86a8fcb0
Gustavo Patrocínio <gustavopatrocinio00@gmail.com>
2019-10-16 10:09:37 -0300

    insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -30,6 +31,10 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("=== TEST 4: seller insert ===");
+		Seller newseller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newseller);
+		System.out.println("Inserted new id = " + newseller.getId());
 	}
 
 }
